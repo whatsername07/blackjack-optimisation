fecbc637f15518988ffbae4a36c353ea25900968
whatsername07 <alexwanklyn@gmail.com>
2026-08-27 19:28:29 +0100

    Added Sock class for dealing and storing cards

--- src/engine.py
+++ src/engine.py
@@ -23,3 +23,27 @@ class Card:
         else:
             self.value = int(rank)
         pass
+
+class Shoe:
+    """Manages multi-deck shoe creation, shuffling and penetration threshold"""
+    def __init__(self, num_decks: int = 6, penetration: float = 0.75):
+        self.num_decks = num_decks
+        self.penetration = penetration
+        self.cards: List[Card] = []
+        self.cut_card_reached = False
+        self.reset_and_shuffle()
+
+    def reset_and_shuffle(self) -> None:
+        ranks = [str(n) for n in range(2,11)] + ["J","Q","K","A"]
+        suits = ["♠", "♥", "♦", "♣"]
+        self.cards = [Card(r,s) for _ in range(self.num_decks) for s in suits for r in ranks]
+        random.shuffle(self.cards)
+        self.cut_card_index = int(len(self.cards) * (1-self.penetration))
+        self.cut_card_reached = False
+
+    def deal_card(self) -> Card:
+        if len(self.cards) <= self.cut_card_index:
+            self.cut_card_reached = True
+        return self.cards.pop()
+
+    
